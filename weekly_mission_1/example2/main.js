8b87da9cf82af193821e4123c1e004a3c28ba629
LuceroLuciano <lucero.luciano@umb.edu.mx>
2022-04-08 19:15:21 -0500

    Exportando funciones entre scripts con CommonJS

---
+++ weekly_mission_1/example2/main.js
@@ -0,0 +1,7 @@
+// node main.js
+
+// hace uso del modulo logger.js
+const logger = require('./logger')
+
+logger.info('This is an informational message')
+logger.verbose('This is a verbose message')
