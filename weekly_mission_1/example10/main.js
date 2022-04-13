37201cdbd3abe490fcc90e86640f1ac47ab9b644
LuceroLuciano <lucero.luciano@umb.edu.mx>
2022-04-13 14:54:19 -0500

    Ejercicio2 de practica usando ESM

---
+++ weekly_mission_1/example10/main.js
@@ -0,0 +1,9 @@
+import MyPokemon from './pokemon.js'
+
+const pikachu = new MyPokemon('Pikachu')
+const chikorita = new MyPokemon('Chikorita')
+const tauros = new MyPokemon('Tauros')
+
+pikachu.sayHello()
+chikorita.sayHello()
+tauros.sayHello()
