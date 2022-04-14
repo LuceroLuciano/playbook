85adf755bbd1f372f61fb74cf4e7eeb919bb0075
LuceroLuciano <lucero.luciano@umb.edu.mx>
2022-04-14 15:02:18 -0500

    2. Ejemplo For Each

--- weekly_mission_2/example2/example2_2.js
+++ weekly_mission_2/example2/example2_2.js
@@ -1,2 +1,18 @@
+// Operadores
 // Ejemplo 2: for Each para calcular la suma de todos los elementos de una lista
-console.log("Ejemplo 2: Calculo de la suma de los elementos de una lista")
+let sum = 0;
+const numbers2 = [1, 2, 3, 4, 5];
+numbers2.forEach(num => sum += num)
+console.log("Ejemplo 2: Calculo de la suma de los elementos de una lista \n")
+console.log(sum)
+
+
+// Imprimiento la suma de los elementos de la lista con un ciclo for
+/*let suma = 0;
+for (num = 0; num < numbers2.length; num++){
+    // suma los elementos de la lista
+    suma += numbers2[num]
+    // console.log(suma)    
+}
+console.log(suma)
+console.log(`La suma de los elementos de la lista es: ${suma}`) */
