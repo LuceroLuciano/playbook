85eb65c41891686dbee6b5ed0bfd9876d9d9eb26
LuceroLuciano <lucero.luciano@umb.edu.mx>
2022-04-13 19:08:46 -0500

    Crear un objeto con propiedades

---
+++ weekly_mission_2/example2/script3.js
@@ -0,0 +1,20 @@
+// Ejemplo 3: Objeto con diferentes propiedades
+const myObject3 = {
+    name: "Raspi",
+    age: 5,
+    nicknames: [
+        "Raspin",
+        "Raspita",
+        "Raspiiii"
+    ],
+    address: {
+        zip_code: "10000",
+        street: "Galaxia, Universo unico",
+        city: "Marte"
+    }
+}
+
+console.log("Ejemplo 3: Objeto con diferentes propiedades")
+console.log(myObject3)
+console.log(myObject3.name)
+console.log(myObject3["address"])
