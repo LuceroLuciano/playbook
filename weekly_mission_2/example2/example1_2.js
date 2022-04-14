15f43e30ed2e37a4233392421292fc543416526f
LuceroLuciano <lucero.luciano@umb.edu.mx>
2022-04-14 14:41:40 -0500

    Operadores

---
+++ weekly_mission_2/example2/example1_2.js
@@ -0,0 +1,9 @@
+// Ejemplo 1: for Each para imprimir elementos de una lista
+const numbers = [1, 2, 3, 4, 5];
+console.log("Ejemplo 1: Imprimiendo los elementos de una lista \n")
+numbers.forEach(num => console.log(num))
+
+// Imprimiendo los elementos de la lista con un ciclo for
+/*for (let num = 0; num < numbers.length; num++){
+    console.log(numbers[num])
+} */
---
+++ weekly_mission_2/example2/example2_2.js
@@ -0,0 +1,2 @@
+// Ejemplo 2: for Each para calcular la suma de todos los elementos de una lista
+console.log("Ejemplo 2: Calculo de la suma de los elementos de una lista")
