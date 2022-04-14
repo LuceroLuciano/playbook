8703b592fbb1c1d2b77b18fe26fbf77db0d2bc1a
LuceroLuciano <lucero.luciano@umb.edu.mx>
2022-04-13 19:08:06 -0500

    Crea un objeto con propiedades

---
+++ weekly_mission_2/example2/script2.js
@@ -0,0 +1,8 @@
+// Ejemplo 2: Crear un objeto con propiedades
+const myObject2 = {
+    name: "Lucero",
+    age: 23
+}
+
+console.log("Ejemplo 2: Crear un objeto con propiedades")
+console.log(myObject2)
