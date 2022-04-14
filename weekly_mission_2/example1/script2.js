ece611efb2f0fc0fdd3e625a9817f30d0ae9b889
LuceroLuciano <lucero.luciano@umb.edu.mx>
2022-04-13 19:54:05 -0500

    Revert "Semana2"
    This reverts commit d66d2919a6a68dfe04ca5207a50a1a57c2007be0.

--- weekly_mission_2/example1/script2.js
+++
@@ -1,8 +0,0 @@
-// Ejemplo 2: Crear un objeto con propiedades
-const myObject2 = {
-    name: "Lucero",
-    age: 23
-}
-
-console.log("Ejemplo 2: Crear un objeto con propiedades")
-console.log(myObject2)
