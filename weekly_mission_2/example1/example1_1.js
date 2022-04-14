ce5cbce25de6fd0542341b19d6ec729ee12c7d96
LuceroLuciano <lucero.luciano@umb.edu.mx>
2022-04-14 13:34:29 -0500

    Ejemplo 1: Crear un objeto vacio

--- weekly_mission_2/example1/example1_1.js
+++ weekly_mission_2/example1/example1_1.js
@@ -1,4 +1,4 @@
 // Ejemplo 1: Esto es un objeto
 const myObject = {} // Esto es un objeto vacio
-console.log("Ejemplo 1: Crear un objeto vacio")
+console.log("Example 1: Crear un objeto vacio")
 console.log(myObject)
