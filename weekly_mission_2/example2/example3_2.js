3bd40e7cfd28cdab14961a2edcf7c490795e7db0
LuceroLuciano <lucero.luciano@umb.edu.mx>
2022-04-14 19:55:23 -0500

    3. Ejemplo For Each

---
+++ weekly_mission_2/example2/example3_2.js
@@ -0,0 +1,16 @@
+// Operadores
+// Ejemplo 3: forEach para imprimir los países en letras mayúsculas
+const countries = ['Finland', 'Canada', 'Japan', 'India', 'Peru']
+console.log("Ejemplo 3: Imprimiendo la lista de paises en mayusculas")
+countries.forEach((element) => console.log(element.toUpperCase())) 
+
+
+
+
+// Imprimiedo la lista de paises en mayusculas
+/*let countriesMayus = ""
+for (element = 0; element < countries.length; element++){
+    // convierte a muyusculas el elemento de la lista
+    countriesMayus = countries[element].toUpperCase()
+    console.log(countriesMayus)
+}*/
