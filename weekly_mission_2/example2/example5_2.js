d8efcb11ded606f13e69c80374eb09029f88dd7e
LuceroLuciano <lucero.luciano@umb.edu.mx>
2022-04-15 19:04:45 -0500

    6. Ejemplo Map

---
+++ weekly_mission_2/example2/example5_2.js
@@ -0,0 +1,12 @@
+// Ejemplo 5: Uso de Map para convertir todos los nombres de una lista a mayusculas y crear una nueva lista
+const names = ['Lucero', 'Carlos', 'Maria', 'Juan', 'Jesus']
+// recorrer la lista y convertir los nombres a mayusculas
+const namesToUpperCase = names.map((name) => name.toUpperCase())
+console.log(namesToUpperCase)
+
+
+
+/*
+// Recorrer la lista y mostrar los elementos
+const namesToUpperCase2 = names.forEach((name) => console.log(name.toUpperCase()))
+*/
