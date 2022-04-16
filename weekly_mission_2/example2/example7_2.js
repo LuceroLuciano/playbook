fee6b4b316b58ca3ea98e692f55fc1c2ee7bc2b4
LuceroLuciano <lucero.luciano@umb.edu.mx>
2022-04-15 19:45:21 -0500

    7. Ejemplo Filter

--- weekly_mission_2/example2/example7_2.js
+++ weekly_mission_2/example2/example7_2.js
@@ -1,5 +1,7 @@
 // Ejemplo 7: Uso de filter para filtrar una lista de elemento
 const countries7 = ['Finland', 'Canada', 'Ingland', 'Ecuador', 'Japan']
+// Filtra los elementos de la lista y muestra solo los que
+// elementos que incluyan land
 const countriesContainingLand = countries7.filter((country) => //Esta es una función
     country.includes('land')
 )
@@ -7,6 +9,7 @@ const countriesContainingLand = countries7.filter((country) => //Esta es una fun
 console.log("Ejemplo 7: Uso de filter para filtrar una lista de elementos")
 console.log(countriesContainingLand)
 
+// Filtra los elementos que tengan ia
 const countriesEndByia = countries7.filter((country) => country.endsWith('ia'))
 console.log("Ejemplo 7: Paises que terminan en ia")
 console.log(countriesEndByia)
