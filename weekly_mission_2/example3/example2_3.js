9cd11638a12ae081f93b8b987e233f16cbcee766
LuceroLuciano <lucero.luciano@umb.edu.mx>
2022-04-16 22:38:50 -0500

    2. Instanciar objetos de una clase

---
+++ weekly_mission_2/example3/example2_3.js
@@ -0,0 +1,13 @@
+// Ejemplo 2: Crear un objeto a partir de una clase.
+
+// Clase
+class Ninja {
+
+}
+
+// Objeto
+const itachi = new Ninja()
+const obito = new Ninja()
+
+console.log(itachi)
+console.log(obito)
