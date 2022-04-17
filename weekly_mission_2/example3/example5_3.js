668fc63080b671718ba12401c571c96ed3170dad
LuceroLuciano <lucero.luciano@umb.edu.mx>
2022-04-16 23:25:13 -0500

    Atributos con valores por default

---
+++ weekly_mission_2/example3/example5_3.js
@@ -0,0 +1,57 @@
+// Ejemplo 5: Atributos con valores por default
+
+class Ninja {
+    constructor(name, age, ninjaRank) {
+        this.name = name
+        this.age = age
+        this.ninjaRank = ninjaRank
+
+        // Atributos con valores por defecto
+        this.aldea = 'Konoha'
+        this.dataCreated = new Date() // Esto guarda la fecha actual en la que se inicia el objeto
+    }
+
+    // Método
+    getInfo() {
+        return `
+            NINJA, ${this.name.toUpperCase()}
+            ${'---'.repeat(10)}
+            Edad: ${this.age}
+            Rango ninja: ${this.ninjaRank.rank}
+            Ascenso a chunin: ${this.ninjaRank.promotionToChunin}
+            Aldea: ${this.aldea}
+            ${'---'.repeat(10)}
+            Fecha de creación: ${this.dataCreated}
+        `
+    }
+}
+
+
+// Instanciar Objeto1
+const itachi = new Ninja(
+    'Itachi',
+    '21',
+    {
+        rank: 'Ambu',
+        record: '012110',
+        promotionToChunin: '10 años'
+    }
+)
+
+// Instanciar objeto2
+const obito = new Ninja(
+    'Obito',
+    '31',
+    {
+        rank: 'Chunin',
+        record: '010886',
+        promotionToChunin: '11 años'
+    }
+)
+
+console.log(itachi)
+console.log(obito)
+
+// Accede al metodo getInfo()
+console.log(itachi.getInfo())
+console.log(obito.getInfo())
