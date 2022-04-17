c57e2c640b1c06468c2a84177b3beabc92031127
LuceroLuciano <lucero.luciano@umb.edu.mx>
2022-04-17 11:10:53 -0500

    8. Metodo static

--- weekly_mission_2/example3/example8_3.js
+++ weekly_mission_2/example3/example8_3.js
@@ -1,72 +1,15 @@
 // Ejemplo 8: Métodos static nos ayudan a escribir métodos en una 
 // clase que podemos usar sin necesidad de instanciar algún objeto.
 
-class Ninja {
-    constructor(name, age, ninjaRank, techniques) {
-        this.name = name
-        this.age = age
-        this.ninjaRank = ninjaRank
-        this.techniques = techniques
-        this.aldea = null
-        this.istAlive = true
-    }
-
-    // Método getter
-    getInfo() {
-        return `
-            NINJA, ${this.name.toUpperCase()}
-            ${'----'.repeat(10)}
-            Edad: ${this.age}
-            Rango ninja: ${this.ninjaRank.rank}
-            Ascenso a chūnin: ${this.ninjaRank.promotionToChunin}
-            Aldea: ${this.aldea}
-            ${'----'.repeat(10)}
-            Fecha de ceación: ${this.dateCreated}
-        `
-    }
-
-    // Método getter
-    get getStatus() {
-        return this.itsAlive
-    }
-
-    // Método getter
-    get getAldea() {
-        return this.aldea
-    }
-
-    // Método setter
-    set setStatus(status) {
-        this.itsAlive = status
-    }   
-
-    // Método setter
-    set setAldea(aldea) {
-        this.aldea = aldea
-    }
-
-    // Método static
-    static getTechniques(techniques) {
-        let template = ``
-        techniques.forEach(function(techniques, index) {
-            template += `${++index}.${techniques}\n`
-        })
-        return template
+class Toolbox {
+    static getMostUsefulTools(){
+        return ['Command line', 'git', 'Text Editor']
     }
 }
 
-const rocklee = new Ninja(
-    'Rock Lee',
-    '32',
-    {
-        rank: 'Jonin',
-        record: '012561',
-        promotionToChunin: '15 años'
-    },
-    ['Gran Remolino de la Hoja', 'Loto Inverso', 'Loto Primario', 'Puño Borracho']
-
-)
-
-
-console.log('Técnicas secretas de', rocklee.name)
-console.log(Ninja.getTechniques(rocklee.techniques))
+console.log("Ejemplo 8: Metodos static")
+// Puedo llamar el método static directamente con el nombre de la clase
+console.log(Toolbox.getMostUsefulTools())
+// Si intentamos instanciar un objeto, no podremos llamar este método static
+//const toolbox = new Toolbox()
+//console.log(toolbox.getMostUsefulTools()) // is not a function
