0fe25137a5fb85859bf845038ae5cfe9d99c9f6e
LuceroLuciano <lucero.luciano@umb.edu.mx>
2022-04-18 11:28:15 -0500

    2. Definicion de la clase hija: viajero.js

--- weekly_mission_2/example4/viajero.js
+++ weekly_mission_2/example4/viajero.js
@@ -1,5 +1,7 @@
+// Importa Clase Padre
 import Explorer from "./explorer.js";
 
+// Crear clase Hija
 export default class Viajero extends Explorer {
     constructor(name, username, mission, cycle){
         super(name, username, mission)
