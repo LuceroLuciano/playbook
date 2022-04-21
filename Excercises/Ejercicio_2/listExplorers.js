2bc63402dc3ab6741da0a81279d45b63319c335f
LuceroLuciano <lucero.luciano@umb.edu.mx>
2022-04-21 12:38:58 -0500

    4. Obten la lista de explorer que tengan en su stack js usando FILTER y el metodo includes

--- Excercises/Ejercicio_2/listExplorers.js
+++ Excercises/Ejercicio_2/listExplorers.js
@@ -57,16 +57,18 @@ const explorers = [
         }
       }
     }
-   ]
+]
 
    // 1. Imprime el nombre de cada explorer en la lista con FOR EACH
    console.log("1. Imprime el nombre de cada explorer en la lista: \n")
    explorers.forEach((nameExplorer) => console.log(nameExplorer.name))
    
+   
    // 2. Imprime el stack de cada explorer usando FOR EACH
    console.log("---".repeat(25))
    console.log("2. Imprime el stack de cada explorer usando FOR EACH: \n")
    explorers.forEach((stackExplorer) => console.log(stackExplorer.stack))
+
 
    // 3. Crea una nueva lista con las listas de stacks de cada explorer usando MAP
    console.log("---".repeat(25))
@@ -74,3 +76,18 @@ const explorers = [
    // MAP recorre una lista y crea una nueva
    const newStackListExplorers = explorers.map(function(stackExplorers){return stackExplorers.stack})
    console.log(newStackListExplorers)
+
+
+   /* Obtén la lista de explorers que tengan en su stack "js", 
+   usa FILTER (para validar un elemento en un lista se usa el método includes)*/
+   console.log("---".repeat(25))
+   console.log('4. Obtén la lista de explorers que tengan en su stack "js",usando FILTER y el método includes')
+  
+   const explorersWithStackJs = explorers.filter((stack) => 
+        stack.stack.includes('js')              
+   )  
+   // explorersWithStackJs.forEach((nameExplorer) => console.log(nameExplorer.name))
+   console.log(explorersWithStackJs)
+
+    
+   
