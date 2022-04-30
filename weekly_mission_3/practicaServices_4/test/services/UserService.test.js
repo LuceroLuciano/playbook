fa312a6b180b1efca02cbf34287b85b8de8d5fbc
LuceroLuciano <lucero.luciano@umb.edu.mx>
2022-04-29 20:13:12 -0500

    Agregando archivos app, test y modificando package.json para correr proyecto con npm start

---
+++ weekly_mission_3/practicaServices_4/test/services/UserService.test.js
@@ -0,0 +1,11 @@
+const UserService = require('./../../test/services/UserService')
+
+describe("Test for UserService", () => {
+    test("1. Create a new user using the UserService", () => {
+        const user = UserService.create(1, "Luz", "Lucero")
+        expect(user.username).toBe("Lucero")
+        expect(user.name).toBe("Lux")
+        expect(user.id).toBe(1)
+        expect(user.bio).not.toBeUndefined()
+    })
+})
